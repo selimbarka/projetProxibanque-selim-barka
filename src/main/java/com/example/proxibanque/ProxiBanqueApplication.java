b4c184dfcbc4f0d8b38e254c08ccae410005220d
Selim.Barka <selim.barka@epita.fr>
2025-11-20 10:38:10 +0100

    projet vide

---
+++ src/main/java/com/example/proxibanque/ProxiBanqueApplication.java
@@ -0,0 +1,13 @@
+package com.example.proxibanque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxiBanqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxiBanqueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/proxibanque/ProxiBanqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proxibanque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxiBanqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
